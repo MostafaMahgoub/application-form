16f0e9a2eb43186299cc35f7b9dce810b900e39a
MostafaR <mostafamahgoub@outlook.com>
2023-09-24 20:36:37 +0300

    First attempt test on the questions addition

--- src/AdditionalQuestionSection/AdditionalQuestionSection.tsx
+++ src/AdditionalQuestionSection/AdditionalQuestionSection.tsx
@@ -1,21 +1,48 @@
-import { Divider } from 'antd';
-import AddQuestionButton from '../MainComponents/AddQuestionButton';
-import FormComponent from '../MainComponents/Form-Component';
-
+import React, { useState } from "react";
+import { Divider } from "antd";
+import AddQuestionButton from "../MainComponents/AddQuestionButton";
+import FormComponent from "../MainComponents/Form-Component";
+import Questions from "../MainComponents/Question";
+import DeleteQuestionButton from "../MainComponents/DeleteQuestionButton";
+interface Question {
+  id: string;
+  // Define other properties of a question
+}
 function AdditionalQuestionSection() {
-  const handleDeleteQuestion = () => {
-    // Temporary onClick event handler that does nothing
+  const [questions, setQuestions] = useState<Question[]>([]);
+  const generateUniqueId = (): string => {
+    // Implement your unique ID generation logic here
+    // For example, you can use a library like uuid or generate a random string
+    return "unique-id";
   };
-
+  const handleAddQuestion = () => {
+    setQuestions([...questions, { id: generateUniqueId() }]);
+  };
+  const handleDeleteQuestion = (question: Question) => {
+    setQuestions(questions.filter((q) => q.id !== question.id));
+  };
   return (
     <div>
-      <FormComponent title={"Additional questions"} content={<>
+      <FormComponent
+        title="Additional questions"
+        content={
+          <>
+            {questions.map((question) => (
+              <div key={question.id}>
+                <Questions
+                  question={question}
+                  onDelete={handleDeleteQuestion}
+                />
+              </div>
+            ))}
             <AddQuestionButton
-              onClick={handleDeleteQuestion}
-              title={"Add a question"}
-            /></>} />
+              onClick={handleAddQuestion}
+              title="Add a question"
+            />
+          </>
+        }
+      />
     </div>
   );
 }
-
 export default AdditionalQuestionSection;
---
+++ src/MainComponents/Question.tsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import { Divider } from 'antd';
+import { EditOutlined, DownOutlined } from '@ant-design/icons';
+import { Dropdown, Menu, Input } from 'antd';
+import CustomTitle from './InfoTitle';
+import DeleteQuestionButton from './DeleteQuestionButton';
+import SaveButton from './SaveButton';
+
+const { TextArea } = Input;
+
+const questionOptions: string[] = [
+  'Paragraph',
+  'ShortAnswer',
+  'Yes/No',
+  'Dropdown',
+  'MultipleChoice',
+  'Date',
+  'Number',
+  'FileUpload',
+  'Video'
+];
+
+interface Question {
+  id: string;
+  // Define other properties of a question
+}
+
+interface QuestionsProps {
+  question: Question;
+  onDelete: (question: Question) => void;
+}
+
+function Questions(props: QuestionsProps) {
+  const [selectedQuestion, setSelectedQuestion] = useState<string>('');
+  const [answer, setAnswer] = useState<string>('');
+
+  const handleQuestionSelect = (question: string) => {
+    setSelectedQuestion(question);
+  };
+
+  const handleAnswerChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setAnswer(e.target.value);
+  };
+
+  const handleDeleteQuestion = () => {
+    props.onDelete(props.question);
+  };
+
+  const questionMenu = (
+    <Menu>
+      {questionOptions.map((question) => (
+        <Menu.Item key={question} onClick={() => handleQuestionSelect(question)}>
+          {question}
+        </Menu.Item>
+      ))}
+    </Menu>
+  );
+
+  return (
+    <div>
+      <Dropdown overlay={questionMenu} trigger={['click']}>
+        <a className="ant-dropdown-link" onClick={(e) => e.preventDefault()}>
+          Select Question <DownOutlined />
+        </a>
+      </Dropdown>
+      {selectedQuestion && (
+        <div>
+          <CustomTitle title={selectedQuestion} />
+          {selectedQuestion === 'Paragraph' || selectedQuestion === 'ShortAnswer' ? (
+            <TextArea rows={4} value={answer} onChange={handleAnswerChange} />
+          ) : (
+            <Input value={answer} onChange={handleAnswerChange} />
+          )}
+        </div>
+      )}
+      <DeleteQuestionButton title="Delete question" onClick={handleDeleteQuestion} />
+      <Divider />
+    </div>
+  );
+}
+
+export default Questions;
