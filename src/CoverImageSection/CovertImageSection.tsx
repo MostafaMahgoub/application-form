8fdf15c0ad0a5b94e897b7941de4dc7c77b60328
MostafaR <mostafamahgoub@outlook.com>
2023-09-24 17:34:16 +0300

    Finish upload cover image part with the best performance

--- src/CoverImageSection/CovertImageSection.tsx
+++ src/CoverImageSection/CovertImageSection.tsx
@@ -1,14 +1,26 @@
+import { useState } from 'react';
 import { message, Upload } from 'antd';
 import FormComponent from '../MainComponents/Form-Component';
 import { UploadOutlined } from '@ant-design/icons';
+import DeleteQuestionButton from '../MainComponents/DeleteQuestionButton';
 
 const { Dragger } = Upload;
 
 function CoverImageSection() {
+  const [previewImage, setPreviewImage] = useState<string | null>(null);
+
   const props = {
     name: 'file',
     multiple: true,
     action: 'https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188',
+    beforeUpload: (file: File) => {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        setPreviewImage(e.target?.result as string);
+      };
+      reader.readAsDataURL(file);
+      return false; // Prevent default upload behavior
+    },
     onChange(info: any) {
       const { status } = info.file;
       if (status !== 'uploading') {
@@ -25,20 +37,31 @@ function CoverImageSection() {
     },
   };
 
+  function handlePhotoDeletion(): void {
+    setPreviewImage(null);
+  }
+
   return (
     <div>
       <FormComponent
         title="Upload cover image"
         content={
-          <Dragger {...props} >
-            <p className="text-3xl" >
-              <UploadOutlined />
-            </p>
-            <p className="ant-upload-text">Upload cover image</p>
-            <p className="ant-upload-hint">
-            16:9 ratio is recommended. Max image size 1mb
-            </p>
-          </Dragger>
+          <>
+            {previewImage ? (
+              <>
+                <img src={previewImage} alt="Preview" style={{ maxWidth: '100%' , marginBottom: '8px' }} />
+                <DeleteQuestionButton title="Delete & re-upload" onClick={handlePhotoDeletion} />
+              </>
+            ) : (
+              <Dragger {...props}>
+                <p className="text-3xl">
+                  <UploadOutlined />
+                </p>
+                <p className="ant-upload-text">Upload cover image</p>
+                <p className="ant-upload-hint">16:9 ratio is recommended. Max image size 1mb</p>
+              </Dragger>
+            )}
+          </>
         }
       />
     </div>
